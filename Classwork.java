30b7ab578d58ba00a409ec09ae459d9379ca6cfb
whatsmyname0 <mikepham2007@gmail.com>
2024-09-26 10:22:06 -0700

    tes

--- Classwork.java
+++ Classwork.java
@@ -2,9 +2,18 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    // test your code here
+    String x = new String("disctionary");
+    int n = 1;
+    firstLastN(x, n);
   }
 
-  // write solutions to problems below
+  public static void firstLastN(String x, int n)
+  {
+    int length = x.length();
+    String str1 = x.substring(0,n);
+    String str2 = x.substring(length - n);
+    System.out.print(str1 + str2);
+  }
+
 
 }
