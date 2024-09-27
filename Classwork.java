3714ae31b74325c0b1cbdbfbc8e6e27e3d0b38ee
whatsmyname0 <mikepham2007@gmail.com>
2024-09-27 09:57:55 -0700

    te

--- Classwork.java
+++ Classwork.java
@@ -2,18 +2,34 @@ public class Classwork
 {
   public static void main(String[] args)
   {
-    String x = new String("disctionary");
+    String x1 = new String("Dictionary");
+    String x2 = new String("apple");
     int n = 1;
-    firstLastN(x, n);
+    firstLastN(x1, n);
+    delEnd(x1, n);
   }
 
-  public static void firstLastN(String x, int n)
+  public static void firstLastN(String x1, int n)
   {
-    int length = x.length();
-    String str1 = x.substring(0,n);
-    String str2 = x.substring(length - n);
-    System.out.print(str1 + str2);
+    int L = x.length();
+    String str1 = x1.substring(0,n);
+    String str2 = x1.substring(L - n);
+    System.out.println(str1 + str2);
   }
 
+  public static void delEnd(String x1, int n)
+  {
+    int L = x1.length();
+    int n1 = (L - n);
+    String str1 = x1.substring(0, n1);
+    System.out.println(str1);
+  }
 
+  public static void compareLower(String x1, String x2)
+  {
+    String str1 = x1.toLowerCase();
+    String str2 = x2.toLowerCase();
+    String output = x1.compareTo(x2);
+    System.out.println(output);
+  }
 }
